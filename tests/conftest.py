cd3cb328b1d5edd9f56ed2247cab7b55078d7637
dnslmnv <leeden18@mail.ru>
2025-12-11 06:02:51 +0300

    123

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-
+print("hi")
 @pytest.fixture
 def driver():
     options = webdriver.ChromeOptions()
